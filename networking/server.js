74d2c9ee2cd353231778a1cd0854838ba77f84a2
MatheusKael <matheuskael.silva@gmail.com>
2022-02-21 11:40:02 -0300

    Feat(Networking): Creating a TCP server and tracking clients

---
+++ networking/server.js
@@ -0,0 +1,20 @@
+const net = require("net");
+let clients = 0;
+
+const server = net.createServer(function (client) {
+  clients++;
+  const clientId = clients;
+  console.log("Client connected: " + clientId);
+
+  client.on("end", () => {
+    console.log("Client disconnected: " + clientId);
+  });
+
+  client.write("Welcome client: " + clientId + "rn");
+
+  client.pipe(client);
+});
+
+server.listen(8000, () => {
+  console.log("Server started on port 8000");
+});
